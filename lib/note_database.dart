adc97adc19445a0cfeae751e6e6a65eb6c6a00c8
horcrux <abdul.aziz.aa008@gmail.com>
2024-12-14 22:42:04 +0700

    first commit

---
+++ lib/note_database.dart
@@ -0,0 +1,28 @@
+import 'package:notes_app/note.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class NoteDatabase {
+  //Instatiate new Database object
+  final database = Supabase.instance.client.from('notes');
+
+  //Insert a note to database
+  Future<void> insertNote(Note note) async {
+    await database.insert(note.toJson());
+  }
+
+  //Get a stream of notes from database
+  Stream<List<Note>> getNotesStream() {
+    return database.stream(primaryKey: ['id']).map(
+        (data) => data.map((json) => Note.fromJson(json)).toList());
+  }
+
+  //update a note in the database
+  Future<void> updateNote(Note note) async {
+    await database.update(note.toJson()).eq('id', note.id!);
+  }
+
+  //Delete a note from database
+  Future<void> deleteNote(Note note) async {
+    await database.delete().eq('id', note.id!);
+  }
+}
